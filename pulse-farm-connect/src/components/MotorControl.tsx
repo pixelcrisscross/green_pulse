cff8266bd44d49c1975bfab943aa07af1588c006
Piyush Annigeri <annigeripiyush1@gmail.com>
2025-12-05 00:18:10 +0530

    Update iframe source URL in MotorControl component

--- pulse-farm-connect/src/components/MotorControl.tsx
+++ pulse-farm-connect/src/components/MotorControl.tsx
@@ -5,7 +5,7 @@ const MotorControl = () => {
   return (
     <Card className="p-6 shadow-soft bg-white max-w-2xl">
       <iframe
-        src="https://172.17.2.160"
+        src="https://agitational-ronan-nonreversed.ngrok-free.dev"
         className="w-full h-[600px] rounded-lg border"
         sandbox="allow-scripts allow-forms allow-same-origin"
       />
